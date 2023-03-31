cbc55e29dfa391e7b978e97a3637863ef0717e97
HrithikBanik <105536031+HrithikBanik@users.noreply.github.com>
2023-03-31 20:10:00 +0530

    init project

---
+++ app/src/main/java/com/example/familyapplication/Homefragment.kt
@@ -0,0 +1,35 @@
+package com.example.familyapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class Homefragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_homefragment, container, false)
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance() = Homefragment()
+
+    }
+
+
+}
